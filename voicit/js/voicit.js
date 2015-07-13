f78bc39bf37090739bd40284bba2368fee9f56a2
Andrew Mazzola <admazzola@gmail.com>
2015-07-13 11:15:38 -0400

    Update voicit.js

--- voicit/js/voicit.js
+++ voicit/js/voicit.js
@@ -7,10 +7,34 @@ function playVoice(string)
   console.log( string );
 }
 
+function test()
+{
+  var url = "http://reddit.com/r/all.json";
+ $.getJSON( url, {
+   tags: "mount rainier",
+   tagmode: "any",
+   format: "json"
+ })
+   .done(function( data ) {
 
+     console.log(data);
+     /*
+     $.each( data.items, function( i, item ) {
+       $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+       if ( i === 3 ) {
+         return false;
+       }
+     });
+     */
+   });
+
+
+}
 function init()
   {
     setInterval(function () { playVoice("voice it") }  , 3000);
+
+    test();
 
   }
 
